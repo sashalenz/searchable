5488b45a651bc9967ac720de2802ca63da737ae5
Oleksandr Petrovskyi <sashalenz@gmail.com>
2022-05-26 15:13:27 +0300

    added filters

--- src/Filters/DaterangeFilter.php
+++ src/Filters/DaterangeFilter.php
@@ -4,38 +4,31 @@ namespace Sashalenz\Searchable\Filters;
 
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 use Spatie\QueryBuilder\Filters\Filter;
 
 class DaterangeFilter implements Filter
 {
-    private string $property;
-
-    public function __construct(string $property)
-    {
-        $this->property = $property;
-    }
-
     public function __invoke(Builder $query, $value = null, $property = null): Builder
     {
-        $range = Str::of($value)->explode(' ');
+        $range = Arr::wrap($value);
         $table = $query->getModel()->getTable();
 
         return $query
             ->when(
-                count($range) === 3,
+                count($range) === 2,
                 fn (Builder $query) => $query->whereBetween(
-                    $table . '.' . is_null($property) ? $this->property : $property,
+                    $table . '.' . $property,
                     [
                         Carbon::createFromFormat('Y-m-d', $range[0])->startOfDay(),
-                        Carbon::createFromFormat('Y-m-d', $range[2])->endOfDay(),
+                        Carbon::createFromFormat('Y-m-d', $range[1])->endOfDay(),
                     ]
                 )
             )
             ->when(
                 count($range) === 1,
                 fn (Builder $query) => $query->whereBetween(
-                    $table . '.' . is_null($property) ? $this->property : $property,
+                    $table . '.' . $property,
                     [
                         Carbon::createFromFormat('Y-m-d', $range[0])->startOfDay(),
                         Carbon::createFromFormat('Y-m-d', $range[0])->endOfDay(),
